4fd3a3b204436a37245a25fa2863733c271401fb
NattiiZ <nat87972@gmail.com>
2025-03-04 16:36:29 +0700

    .

--- index.js
+++ index.js
@@ -346,6 +346,29 @@ const OrderDetails = sequelize.define("OrderDetails", {
 });
 
 
+// // ================== Relationships ==================
+
+// // ความสัมพันธ์ระหว่าง Customers และ Orders
+// Customers.hasMany(Orders, { foreignKey: "customer_ID" });
+// Orders.belongsTo(Customers, { foreignKey: "customer_ID" });
+
+// // ความสัมพันธ์ระหว่าง Orders และ OrderDetail
+// Orders.hasMany(OrderDetails, { foreignKey: "order_ID" });
+// OrderDetails.belongsTo(Orders, { foreignKey: "order_ID" });
+
+// // ความสัมพันธ์ระหว่าง Products และ Category
+// Products.belongsTo(Category, { foreignKey: "category_ID" });
+// Category.hasMany(Products, { foreignKey: "category_ID" });
+
+// // ความสัมพันธ์ระหว่าง Products และ Suppliers
+// Products.belongsTo(Suppliers, { foreignKey: "supplier_ID" });
+// Suppliers.hasMany(Products, { foreignKey: "supplier_ID" });
+
+// // ความสัมพันธ์ระหว่าง OrderDetails และ Products
+// OrderDetails.belongsTo(Products, { foreignKey: "product_ID" });
+// Products.hasMany(OrderDetails, { foreignKey: "product_ID" });
+
+
 // ================== Database Sync ==================
 
 sequelize.sync();
