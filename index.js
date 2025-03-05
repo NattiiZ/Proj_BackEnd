f66effb29f22b63a2facf8ed4caf4378a9049151
NattiiZ <nat87972@gmail.com>
2025-03-05 11:58:27 +0700

    .

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const port = process.env.SERVER_PORT || 3000;
 
 const express = require("express");
 const session = require("express-session");
+
 const DB = require("./models").DB;
 
-const userTypeRoutes = require("./routes/userType.routes");
+const CartItems_Routes = require('./routes/CartItems_route');
+const Category_Routes = require('./routes/Category_route');
+const Brands_Routes = require('./routes/Brands_route');
+const Cart_Routes = require('./routes/Cart_route');
+const Customers_Routes = require('./routes/Customers_route');
+const OrderDetails_Routes = require('./routes/OrderDetails_route');
+const Orders_Routes = require('./routes/Orders_route');
+const Products_Routes = require('./routes/Products_route');
+const Status_Routes = require('./routes/Status_route');
+const Suppliers_Routes = require('./routes/Suppliers_route');
+const UserType_Routes = require('./routes/UserType_route');
+const Users_Routes = require('./routes/Users_route');
 
 const app = express();
-
 
 // app.use(express.json());
 // app.use(session({
@@ -19,7 +30,6 @@ const app = express();
 // }));
 
 
-app.use("/usertype", userTypeRoutes);
 
 
 DB.sync({ alter: true }).then(() => 
