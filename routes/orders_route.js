fc6053cf7b3df37bfc13bef5768ad07e5df314e6
NattiiZ <nat87972@gmail.com>
2025-03-05 17:10:36 +0700

    .

--- routes/orders_route.js
+++ routes/orders_route.js
@@ -5,7 +5,7 @@ const Orders_Controller = require('../controllers/Orders_controller');
 
 router.post('/', Orders_Controller.createOrder);
 router.get('/', Orders_Controller.getOrders);
-router.get('/:id', Orders_Controller.getOrderbyId);
+router.get('/:id', Orders_Controller.getOrderById);
 router.put('/:id', Orders_Controller.updateOrder);
 router.delete('/:id', Orders_Controller.deleteOrder);
 
--- routes/users_route.js
+++ routes/users_route.js
@@ -5,7 +5,7 @@ const Users_Controller = require('../controllers/Users_controller');
 
 router.post('/', Users_Controller.createUser);
 router.get('/', Users_Controller.getUsers);
-outer.get('/:id', Users_Controller.getUserbyId);
+router.get('/:id', Users_Controller.getUserById);
 router.put('/:id', Users_Controller.updateUser);
 router.delete('/:id', Users_Controller.deleteUser);
 
